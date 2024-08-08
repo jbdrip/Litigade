7d5289c65e08582530ef72af63f07fb6bc05f68f
Javier Bran <javierbran73@gmail.com>
2024-08-08 11:45:12 -0600

    estilos completos footer

--- litigade/src/components/Footer.tsx
+++ litigade/src/components/Footer.tsx
@@ -3,85 +3,83 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-12 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col items-start">
-            <h3 className="text-2xl font-bold">Litigade</h3>
-            <div className="mt-4 flex space-x-4">
-              <a href="#" className="text-blue-600 hover:text-blue-800">
-                <FaFacebookF size={20} />
-              </a>
-              <a href="#" className="text-pink-500 hover:text-pink-700">
-                <FaInstagram size={20} />
-              </a>
-              <a href="#" className="text-blue-400 hover:text-blue-600">
-                <FaTwitter size={20} />
-              </a>
-              <a href="#" className="text-red-600 hover:text-red-800">
-                <FaYoutube size={20} />
-              </a>
-            </div>
+    <footer className="bg-[#252B42] py-12 text-white md:relative">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-20 px-6">
+        <div className="flex flex-col md:flex-row justify-between md:items-center mb-8 md:border-b border-gray-700 pb-8">
+          <div className="text-2xl font-bold mb-4 md:mb-0">Litigade</div>
+          <div className="flex space-x-4">
+            <a href="#" className="text-blue-600 hover:text-blue-800">
+              <FaFacebookF size={20} />
+            </a>
+            <a href="#" className="text-pink-500 hover:text-pink-700">
+              <FaInstagram size={20} />
+            </a>
+            <a href="#" className="text-blue-400 hover:text-blue-600">
+              <FaTwitter size={20} />
+            </a>
+            <a href="#" className="text-red-600 hover:text-red-800">
+              <FaYoutube size={20} />
+            </a>
           </div>
         </div>
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-8 pt-6">
           <div>
-            <h4 className="font-bold mb-2">Company Info</h4>
+            <h4 className="font-bold mb-4">Company Info</h4>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">About Us</a></li>
-              <li><a href="#" className="hover:underline">Carrier</a></li>
-              <li><a href="#" className="hover:underline">We are hiring</a></li>
-              <li><a href="#" className="hover:underline">Blog</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">About Us</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Carrier</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">We are hiring</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Blog</a></li>
             </ul>
           </div>
           <div>
-            <h4 className="font-bold mb-2">Legal</h4>
+            <h4 className="font-bold mb-4">Legal</h4>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">About Us</a></li>
-              <li><a href="#" className="hover:underline">Carrier</a></li>
-              <li><a href="#" className="hover:underline">We are hiring</a></li>
-              <li><a href="#" className="hover:underline">Blog</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">About Us</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Carrier</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">We are hiring</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Blog</a></li>
             </ul>
           </div>
           <div>
-            <h4 className="font-bold mb-2">Features</h4>
+            <h4 className="font-bold mb-4">Features</h4>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">Business Marketing</a></li>
-              <li><a href="#" className="hover:underline">User Analytic</a></li>
-              <li><a href="#" className="hover:underline">Live Chat</a></li>
-              <li><a href="#" className="hover:underline">Unlimited Support</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Business Marketing</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">User Analytic</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Live Chat</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Unlimited Support</a></li>
             </ul>
           </div>
           <div>
-            <h4 className="font-bold mb-2">Resources</h4>
+            <h4 className="font-bold mb-4">Resources</h4>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">IOS & Android</a></li>
-              <li><a href="#" className="hover:underline">Watch a Demo</a></li>
-              <li><a href="#" className="hover:underline">Customers</a></li>
-              <li><a href="#" className="hover:underline">API</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">IOS & Android</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Watch a Demo</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">Customers</a></li>
+              <li><a href="#" className="hover:underline text-sm text-input-border-color">API</a></li>
             </ul>
           </div>
-          <div>
-            <h4 className="font-bold mb-2">Get In Touch</h4>
-            <form className="flex space-x-2">
+          <div className="lg:ml-auto">
+            <h4 className="font-bold mb-4">Get In Touch</h4>
+            <form className="flex border-white">
               <input
                 type="email"
-                className="w-full py-2 px-4 rounded-md text-gray-800"
+                className="w-3/5 py-3 px-4 rounded-l-md text-gray-800 text-xs border border-white"
                 placeholder="Your Email"
               />
               <button
                 type="submit"
-                className="py-2 px-4 bg-yellow-500 text-white rounded-md"
+                className="w-2/5 py-3 px-4 bg-[#D0A144] text-white text-xs rounded-r-md border border-white"
               >
                 Subscribe
               </button>
             </form>
-            <p className="mt-4 text-gray-400">Lorem ipsum dolor amit</p>
+            <p className="mt-2 text-gray-400 text-xs">Lorem ipsum dolor amit</p>
           </div>
         </div>
       </div>
-      <div className="mt-8 bg-blue-600 py-4 text-center">
-        <p>Made With Love By Figmaland All Right Reserved</p>
+      <div className="md:absolute bottom-0 w-full bg-gradient-to-r from-[#0072B5] to-[#295C7A] py-4 px-4 text-center md:text-left">
+        <p className='text-xs font-bold py-3 px-24'>Made With Love By Figmaland All Right Reserved</p>
       </div>
     </footer>
   );
